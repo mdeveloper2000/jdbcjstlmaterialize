a2af915d00dd65b1c429628690f2a98ec0eefa11
darkbasic2000 <marcio.vieira.scea@gmail.com>
2022-10-15 11:16:21 -0400

    Script para a tabela do banco de dados

---
+++ database.sql
@@ -0,0 +1,7 @@
+CREATE TABLE softwares (
+    ID INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    versao VARCHAR(20) NOT NULL,
+    data_lancamento DATE NOT NULL,
+    PRIMARY KEY(id)
+);
