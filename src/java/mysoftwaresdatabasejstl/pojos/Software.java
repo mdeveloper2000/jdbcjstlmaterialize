99c334f28d6ba1fddc5a11a90919095480207f36
darkbasic2000 <marcio.vieira.scea@gmail.com>
2022-10-15 11:35:36 -0400

    Classe POJO utilizada no projeto

---
+++ src/java/mysoftwaresdatabasejstl/pojos/Software.java
@@ -0,0 +1,44 @@
+package mysoftwaresdatabasejstl.pojos;
+
+import java.util.Date;
+
+public class Software {
+
+    private int id;
+    private String nome;
+    private String versao;
+    private Date data_lancamento;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getVersao() {
+        return versao;
+    }
+
+    public void setVersao(String versao) {
+        this.versao = versao;
+    }
+
+    public Date getData_lancamento() {
+        return data_lancamento;
+    }
+
+    public void setData_lancamento(Date data_lancamento) {
+        this.data_lancamento = data_lancamento;
+    }
+    
+}
